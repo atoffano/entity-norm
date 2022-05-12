e90180427d6eb19a17c65c88f56bba267d04898a
atoffano <antoine.toffano@gmail.com>
2022-05-12 10:16:59 +0200

    docstring generate_dict_BioSyn

--- utils/generate_dictionnary_BioSyn.py
+++ utils/generate_dictionnary_BioSyn.py
@@ -3,6 +3,9 @@ import argparse, os
 import glob
 
 def main():
+    '''
+    Takes in .obo file of OntoBiotope annotations used to makes the bacteria biotope 4 dataset and outputs a dictionnary in BioSyn format.
+    '''
     raw = "/home/atoffano/Downloads/bb4_dictionnary/OntoBiotope_BioNLP-OST-2019.obo"
     with open(raw, 'r') as fh:
         lines = fh.readlines()
