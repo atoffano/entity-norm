285ce1f8f99a5f42ab67dd5b73b94377a46c6fa2
atoffano <antoine.toffano@gmail.com>
2022-07-08 17:33:58 +0200

    chmod lw fix path

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -22,6 +22,6 @@ rm Biomedical-Entity-Linking/source/train.py
 rm Biomedical-Entity-Linking/source/generate_candidate.py
 rm Biomedical-Entity-Linking/source/load_data.py
 cp -r utils/mods/Lightweight/input/* Biomedical-Entity-Linking/input
-chmod -R +x Biomedical-Entity-Linking/source/input/Ab3P/identify_abbr
+chmod -R +x Biomedical-Entity-Linking/input/Ab3P/identify_abbr
 cp -r utils/mods/Lightweight/source/* Biomedical-Entity-Linking/source
 echo Lightweight init done!
